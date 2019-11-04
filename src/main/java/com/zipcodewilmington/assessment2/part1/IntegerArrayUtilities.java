0c237427cc293671deed5d818ba77b6bcfce749f
Kai Shields <kai@zipcoders-MacBook-Pro-3.local>
2019-11-04 14:08:59 -0500

    checkpoint

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,29 +1,44 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import com.j256.ormlite.stmt.query.In;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
         Boolean result = false;
         if (array.length % 2 == 0) {
             return true;
-        }
-        else{
-           return result;
+        } else {
+            return result;
         }
     }
 
     public Integer[] range(int start, int stop) {
-return null;
+        List<Integer> list = new LinkedList();
+        for (int i = start; i <= stop; i++) {
+            list.add(i);
+        }
+        Integer[] result = list.toArray(new Integer[list.size()]);
+        return result;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-          int sum = 0;
-            for (int i = 0; i <= 2; i++) {
-                sum += i;
-            }
-            return sum;
+        int sum = 0;
+        for (int i = 0; i <= 1; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        int product = 1;
+        for (int i = array.length - 2; i < array.length; i++) {
+                product = product * array[i];
+            }
+        return product;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -21,6 +21,7 @@ public class ListUtility {
     }
 
     public Integer mostCommon() {
+
         return null;
     }
 
